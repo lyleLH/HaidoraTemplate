ba9ce3c4e8ed56b255f9b76383fac4532807c344
Tom.Liu <tomliu@yeahka.com>
2021-03-05 18:13:59 +0800

    --宏定义和工具

--- templates/ios/Example/Source/Common/Utility/PROJECTUtility.h
+++ templates/ios/Example/Source/Common/Utility/PROJECTUtility.h
@@ -11,7 +11,9 @@
 
 //该目录下存放属于函数或算法，不是对象而且很多地方能用到，比如排序/加密算法。
 
+#import "PROJECTUtilityMacros.h"
 #import "PROJECTStringUtility.h"
 #import "PROJECTDateUtility.h"
+#import "PROJECTDeviceUtility.h"
 
 #endif
